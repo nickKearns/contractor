fcd6ca31bc635564e17f06e942230ad2ccc3108d
nickKearns <nickkearns133@gmail.com>
2019-10-10 11:42:49 -0700

    can create album

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -24,5 +24,29 @@ def index():
 def albums_index():
     '''display all albums'''
     return render_template('albums_index.html', albums=albums.find())
+
+@app.route('/albums', methods=['POST'])
+def submit_album():
+    '''submit a new album'''
+    album = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'year_released': request.form.get('year_released'),
+        'category': request.form.get('category')
+    }
+    albums.insert_one(album)
+    print(request.form.to_dict())
+    return redirect(url_for('albums_index'))
+
+
+@app.route('/album/new')
+def create_album():
+    '''create a new album'''
+    return render_template('album_new.html')
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)

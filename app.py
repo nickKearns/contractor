fd7ad4c6c358a128e42812c0e2707213674f8efa
nickKearns <nickkearns133@gmail.com>
2019-10-10 10:47:37 -0700

    users can see all albums

--- app.py
+++ app.py
@@ -2,10 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+albums = [
+    {'title': 'Led Zeppelin III', 'description': 'Third self titled led zeppelin album', 'category': 'classic rock'},
+    {'title': 'Led Zeppelin II', 'description': 'Second self titled led zeppelin album', 'category': 'classic rock'}
+]
+
+
+
 @app.route('/')
 def index():
     """Return homepage."""
     return render_template('home.html', msg = 'Nicks Records')
 
+@app.route('/albums')
+def albums_index():
+    '''display all albums'''
+    return render_template('albums_index.html', albums=albums)
 if __name__ == '__main__':
     app.run(debug=True)
